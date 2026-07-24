0b859b47d8e0396f02a6c6817146994a3610030f
Sidhartha Sekhar Padhihari <sidhartha1304@gmail.com>
2026-07-24 18:28:50 +0530

    Time: 24 ms (9.72%), Space: 12.4 MB (54.5%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+         if x < 0:
+            return False
+         reversed = 0
+         original=x
+         while x != 0:
+                digit = x % 10
+                reversed = reversed * 10 + digit
+                x //= 10
+         return original == reversed
